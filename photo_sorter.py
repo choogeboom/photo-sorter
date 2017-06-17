48c361be3d5a5f75a2fbfb5c7ce74a6df870eb97
hoogamaphone <chris.hoogeboom@gmail.com>
2017-06-17 12:32:30 -0400

    started improving sorting and date inference.

--- photo_sorter.py
+++ photo_sorter.py
@@ -1,54 +1,92 @@
-import pathlib as p
+import pathlib as pl
 import datetime
+import re
+from typing import Union, TypeVar
 
 from exifread import process_file
 
 sorted_files = set()
 
+PathLike = Union[str, bytes, pl.Path]
+T = TypeVar('T')
+Maybe = Union[T, None]
 
-def sort_file(path: p.Path, base_path: p.Path) -> bool:
-    if isinstance(path, str):
-        path = p.Path(path)
-    date = get_image_date(path)
-    new_path = (base_path / date.strftime('%Y') / date.strftime('%Y-%m')
+
+def sort_directory(source_path: pl.Path,
+                   target_path: pl.Path,
+                   remove_empty: bool=True,
+                   recursive=True):
+    assert source_path.is_dir()
+    assert target_path.is_dir()
+    if recursive:
+        glob_pattern = '**/*.*'
+    else:
+        glob_pattern = '*.*'
+
+
+def sort_file(path: pl.Path, target_path: PathLike,
+              ) -> Maybe[pl.Path]:
+    if not isinstance(path, pl.Path):
+        path = pl.Path(str(path))
+    date = get_file_date(path)
+    if not date:
+        return None
+    new_path = (target_path / date.strftime('%Y') / date.strftime('%Y-%m')
                 / date.strftime('%Y-%m-%d') / path.name)
     if new_path != path:
         if not new_path.parent.exists():
             new_path.parent.mkdir(parents=True)
         make_backup(new_path)
         path.rename(new_path)
-        if is_empty_dir(path.parent):
-            path.parent.rmdir()
-        return True
+        return new_path
     else:
-        return False
-
+        return None
 
 
 def make_backup(new_path):
     pass
 
 
-def is_empty_dir(path: p.Path):
+def is_empty_dir(path: pl.Path):
     if path.is_dir():
         return sum(1 for _ in path.iterdir()) == 0
     else:
         return False
 
 
-def get_image_date(path: p.Path) -> datetime.date:
-    date_tag_names = ('EXIF DateTimeOriginal', 'EXIF DateTimeDigitized',
-                      'Image DateTime')
-    tags = load_tags(path)
-    for tag in date_tag_names:
-        if tag in tags:
-            date_str = tags[tag].values
-            dt = datetime.datetime.strptime(date_str, '%Y:%m:%d %H:%M:%S')
-            return dt.date()
+def get_file_date(path: pl.Path) -> Maybe[datetime.date]:
+    return (get_file_date_from_metadata(path)
+            or get_file_date_from_filename(path)
+            or get_file_date_from_properties(path))
+
+
+def get_file_date_from_metadata(path: pl.Path) -> Maybe[datetime.date]:
+    if path.suffix in ('jpg', 'jpeg'):
+        date_tag_names = ('EXIF DateTimeOriginal', 'EXIF DateTimeDigitized',
+                          'Image DateTime')
+        tags = load_tags(path)
+        for tag in date_tag_names:
+            if tag in tags:
+                date_str = tags[tag].values
+                dt = datetime.datetime.strptime(date_str, '%Y:%m:%d %H:%M:%S')
+                return dt.date()
+    return None
+
+
+def get_file_date_from_filename(path: pl.Path) -> Maybe[datetime.date]:
+    pattern = r'(?P<year>\d{4})(.|\s+)?(?P<month>\d{2})(.|\s+)?(?P<day>\d{2})'
+    match = re.search(pattern, path.name)
+    if match:
+        date_kwargs = {k: int(v) for k, v in match.groupdict().items()}
+        return datetime.date(**date_kwargs)
     else:
-        raise ValueError(f'Unable to extract date from image {path!s}')
+        return None
 
 
-def load_tags(path: p.Path) -> dict:
+def get_file_date_from_properties(path: pl.Path) -> Maybe[datetime.date]:
+    return None
+
+
+def load_tags(path: pl.Path) -> dict:
     with open(path, 'rb') as image_file:
         return process_file(image_file)
--- tests/test_photo_sorter.py
+++ tests/test_photo_sorter.py
@@ -1,11 +1,11 @@
 import datetime
-import pathlib
+import pathlib as pl
 
 import py
 import pytest
 import photo_sorter as ps
 
-test_file_dir = pathlib.Path('tests/files')
+test_file_dir = pl.Path('tests/files')
 
 
 def _copy(self, target):
@@ -15,43 +15,53 @@ def _copy(self, target):
 
 
 # noinspection PyUnresolvedReferences
-pathlib.Path.copy = _copy
+pl.Path.copy = _copy
 
 
-@pytest.fixture(scope='function')
-def image_tempdir(tmpdir: py.path.local):
-    photos_dir: pathlib.Path = tmpdir.mkdir('photos')
+@pytest.fixture(scope='function', name='image_tempdir')
+def make_image_tempdir(tmpdir: py.path.local) -> pl.Path:
+    return pl.Path(tmpdir.mkdir('photos'))
+
+
+@pytest.fixture(scope='function', name='unsorted_tempdir')
+def make_unsorted_tempdir(image_tempdir: pl.Path) -> pl.Path:
+    unsorted_dir = image_tempdir / 'unsorted'
+    unsorted_dir.mkdir()
     for image_path in test_file_dir.iterdir():
-        new_image_path = photos_dir / image_path.name
+        new_image_path = unsorted_dir / image_path.name
         image_path.copy(new_image_path)
-    return pathlib.Path(photos_dir)
+    return unsorted_dir
 
 
-# noinspection PyShadowingNames
 @pytest.fixture(scope='function',
                 params=['2016_01_01', '2016_04_01', '2017_01_01', '2017_01_11',
-                        '2017_02_12'])
-def test_image(image_tempdir: pathlib.Path, request):
-    test_image_path = image_tempdir / f'test_image_{request.param}.jpeg'
+                        '2017_02_12'],
+                name='test_image')
+def get_test_image(unsorted_tempdir: pl.Path, request):
+    test_image_path = unsorted_tempdir / f'test_image_{request.param}.jpeg'
     date = datetime.datetime.strptime(request.param, '%Y_%m_%d').date()
     return {'path': test_image_path,
             'date': date}
 
 
-# noinspection PyShadowingNames
 def test_get_image_date(test_image: dict):
-    date = ps.get_image_date(test_image['path'])
+    date = ps.get_file_date(test_image['path'])
     assert date == test_image['date']
 
 
-# noinspection PyShadowingNames
-def test_is_empty_dir(image_tempdir):
-    assert not ps.is_empty_dir(image_tempdir)
-    empty_dir = image_tempdir / 'empty'
+def test_is_empty_dir(unsorted_tempdir):
+    assert not ps.is_empty_dir(unsorted_tempdir)
+    empty_dir = unsorted_tempdir / 'empty'
     empty_dir.mkdir()
     assert ps.is_empty_dir(empty_dir)
 
 
-# noinspection PyShadowingNames
-def test_sort_image(image_tempdir: pathlib.Path, test_image: dict):
-    ps.sort_file(test_image['path'], image_tempdir)
+def test_sort_image(image_tempdir: pl.Path,
+                    test_image: dict):
+    new_path = ps.sort_file(test_image['path'], image_tempdir)
+    date: datetime.date = test_image['date']
+    expected_path = (image_tempdir / date.strftime('%Y') / date.strftime('%Y-%m')
+                     / date.strftime('%Y-%m-%d') / test_image['path'].name)
+    assert new_path == expected_path
+    assert ps.sort_file(new_path, image_tempdir) is None
+
