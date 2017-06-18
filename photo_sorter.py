1110c8434625ba7d9248412bffa47395af9f6edd
hoogamaphone <chris.hoogeboom@gmail.com>
2017-06-17 23:12:11 -0400

    sort directory is working.

--- photo_sorter.py
+++ photo_sorter.py
@@ -1,11 +1,10 @@
+import os
 import pathlib as pl
 import datetime
 import re
 from typing import Union, TypeVar
 
 from exifread import process_file
-
-sorted_files = set()
 
 PathLike = Union[str, bytes, pl.Path]
 T = TypeVar('T')
@@ -22,29 +21,52 @@ def sort_directory(source_path: pl.Path,
         glob_pattern = '**/*.*'
     else:
         glob_pattern = '*.*'
+    sorted_files = set()
+    directories_to_delete = set()
+    for file in source_path.glob(glob_pattern):
+        if file.name not in sorted_files:
+            new_file = sort_file(file, target_path)
+            if new_file:
+                sorted_files.add(new_file)
+            if remove_empty and is_empty_dir(file.parent):
+                directories_to_delete.add(file.parent)
+    for directory in directories_to_delete:
+        if is_empty_dir(directory):
+            directory.rmdir()
 
 
 def sort_file(path: pl.Path, target_path: PathLike,
               ) -> Maybe[pl.Path]:
     if not isinstance(path, pl.Path):
         path = pl.Path(str(path))
+
     date = get_file_date(path)
-    if not date:
-        return None
-    new_path = (target_path / date.strftime('%Y') / date.strftime('%Y-%m')
-                / date.strftime('%Y-%m-%d') / path.name)
+    if date:
+        new_path = (target_path / date.strftime('%Y') / date.strftime('%Y-%m')
+                    / date.strftime('%Y-%m-%d') / path.name)
+    else:
+        new_path = target_path / 'Unknown' / path.name
+
     if new_path != path:
         if not new_path.parent.exists():
             new_path.parent.mkdir(parents=True)
-        make_backup(new_path)
+        if new_path.exists():
+            new_path = get_backup_path(new_path)
         path.rename(new_path)
-        return new_path
-    else:
-        return None
+    return new_path
 
 
-def make_backup(new_path):
-    pass
+def get_backup_path(path: pl.Path):
+    def make_path(source_path: pl.Path, number: int):
+        return source_path.with_name(
+            f'{source_path.stem}_{number}.{source_path.suffix}')
+
+    backup_count = 0
+    new_path = make_path(path, backup_count)
+    while new_path.exists():
+        backup_count += 1
+        new_path = make_path(path, backup_count)
+    return new_path
 
 
 def is_empty_dir(path: pl.Path):
@@ -56,8 +78,7 @@ def is_empty_dir(path: pl.Path):
 
 def get_file_date(path: pl.Path) -> Maybe[datetime.date]:
     return (get_file_date_from_metadata(path)
-            or get_file_date_from_filename(path)
-            or get_file_date_from_properties(path))
+            or get_file_date_from_filename(path))
 
 
 def get_file_date_from_metadata(path: pl.Path) -> Maybe[datetime.date]:
@@ -83,8 +104,8 @@ def get_file_date_from_filename(path: pl.Path) -> Maybe[datetime.date]:
         return None
 
 
-def get_file_date_from_properties(path: pl.Path) -> Maybe[datetime.date]:
-    return None
+def get_file_date_from_mtime(path: pl.Path) -> Maybe[datetime.date]:
+    return datetime.date.fromtimestamp(os.path.getmtime(path))
 
 
 def load_tags(path: pl.Path) -> dict:
--- tests/test_photo_sorter.py
+++ tests/test_photo_sorter.py
@@ -34,14 +34,18 @@ def make_unsorted_tempdir(image_tempdir: pl.Path) -> pl.Path:
 
 
 @pytest.fixture(scope='function',
-                params=['2016_01_01', '2016_04_01', '2017_01_01', '2017_01_11',
-                        '2017_02_12'],
+                params=[('2014_04_21', datetime.date(2014, 4, 21)),
+                        ('2016_01_01', datetime.date(2016, 1, 1)),
+                        ('2016_04_01', datetime.date(2016, 4, 1)),
+                        ('2017_01_01', datetime.date(2017, 1, 1)),
+                        ('2017_01_11', datetime.date(2017, 1, 11)),
+                        ('2017_02_12', datetime.date(2017, 2, 12)),
+                        ('no_date', None)],
                 name='test_image')
 def get_test_image(unsorted_tempdir: pl.Path, request):
-    test_image_path = unsorted_tempdir / f'test_image_{request.param}.jpeg'
-    date = datetime.datetime.strptime(request.param, '%Y_%m_%d').date()
+    test_image_path = unsorted_tempdir / f'test_image_{request.param[0]}.jpeg'
     return {'path': test_image_path,
-            'date': date}
+            'date': request.param[1]}
 
 
 def test_get_image_date(test_image: dict):
@@ -60,8 +64,22 @@ def test_sort_image(image_tempdir: pl.Path,
                     test_image: dict):
     new_path = ps.sort_file(test_image['path'], image_tempdir)
     date: datetime.date = test_image['date']
-    expected_path = (image_tempdir / date.strftime('%Y') / date.strftime('%Y-%m')
-                     / date.strftime('%Y-%m-%d') / test_image['path'].name)
+    if date:
+        expected_path = (image_tempdir / date.strftime('%Y') / date.strftime('%Y-%m')
+                         / date.strftime('%Y-%m-%d') / test_image['path'].name)
+    else:
+        expected_path = image_tempdir / 'Unknown' / test_image['path'].name
     assert new_path == expected_path
-    assert ps.sort_file(new_path, image_tempdir) is None
+    assert ps.sort_file(new_path, image_tempdir) == new_path
+
+
+def count_files(path: pl.Path):
+    if path.is_dir():
+        return sum(count_files(p) for p in path.iterdir())
+    else:
+        return 1
+
+
+def test_sort_directory(unsorted_tempdir: pl.Path, image_tempdir: pl.Path):
+    ps.sort_directory(unsorted_tempdir, image_tempdir)
 
